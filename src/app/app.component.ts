6c9e422738d3ffef7f2ac5dd64dbef68bec36c65
DevLeoFulco <devleofulco@gmail.com>
2025-02-23 00:02:25 -0300

    feat: ajustes de renderização de cards para diferentes tipos de areas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ProdutoCardComponent } from './components/produto-card/produto-card.component';
+import { Produto } from './interfaces/produto.interface';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,48 @@ import { ProdutoCardComponent } from './components/produto-card/produto-card.com
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dev-marketplace';
+  produtos: Produto[] = [
+    {
+      id: 1,
+      name: 'Desenvolvedor Full Stack',
+      price: 150.00,
+      image: 'https://via.placeholder.com/300x200',
+      description: 'Desenvolvedor Full Stack com experiência em Angular e Java (8 +)'
+    },
+    {
+      id: 2,
+      name: 'Desenvolvedor BackEnd',
+      price: 120.00,
+      image: 'https://via.placeholder.com/300x200',
+      description: 'Desenvolvedor BackEnd com experiência em Java 8'
+    },
+    {
+      id: 3,
+      name: 'Desenvolvedor Frontend',
+      price: 100.00,
+      image: 'https://via.placeholder.com/300x200',
+      description: 'Desenvolvedor Frontend com experiência em Angular e React'
+    },
+    {
+      id: 4,
+      name: 'Desenvolvedor Mobile',
+      price: 130.00,
+      image: 'https://via.placeholder.com/300x200',
+      description: 'Desenvolvedor Mobile com experiência em React Native e Flutter'
+    },
+    {
+      id: 5,
+      name: 'Arquiteto de Software',
+      price: 200.00,
+      image: 'https://via.placeholder.com/300x200',
+      description: 'Arquiteto de Software com vasta experiência em sistemas distribuídos'
+    },
+    {
+      id: 6,
+      name: 'DevOps Engineer',
+      price: 180.00,
+      image: 'https://via.placeholder.com/300x200',
+      description: 'DevOps Engineer com experiência em AWS e Azure'
+    }
+  ];
 }
--- src/app/components/produto-card/produto-card.component.ts
+++ src/app/components/produto-card/produto-card.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Produto } from '../../interfaces/produto.interface';
 
 @Component({
@@ -9,12 +9,6 @@ import { Produto } from '../../interfaces/produto.interface';
   templateUrl: './produto-card.component.html',
   styleUrl: './produto-card.component.css'
 })
-export class ProdutoCardComponent {
-  produto: Produto = {
-    id: 1,
-    name: 'Desenvolvedor Full Stack',
-    price: 150.00,
-    image: 'https://via.placeholder.com/300x200',
-    description: 'Desenvolvedor Full Stack com experiência em Angular e Java (8 +)'
-  };
+export class ProdutoCardComponent {  
+    @Input() produto!: Produto;
 }
